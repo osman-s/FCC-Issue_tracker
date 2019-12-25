c6bbe8f236332741eac3ab91dfd54d67270e4e36
Osman S <osmanshasan@gmail.com>
2019-12-25 14:00:24 -0500

    removed some template components and updated navbar

---
+++ Client/src/components/issueTracker.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+
+class IssueTracker extends Component {
+  state = {
+    movies: []
+  };
+
+
+
+  render() {
+
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+}
+
+export default IssueTracker;
--- Client/src/components/navBar.jsx
+++ Client/src/components/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Vidly
+        Issue Tracker
       </Link>
       <button
         className="navbar-toggler"
@@ -20,35 +20,12 @@ const NavBar = ({ user }) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/movies">
-            Movies
+          <NavLink className="nav-item nav-link" to="/issuetracker">
+            Home
           </NavLink>
-          <NavLink className="nav-item nav-link" to="/customers">
-            Customers
+          <NavLink className="nav-item nav-link" to="/projectissues">
+            Project Issues
           </NavLink>
-          <NavLink className="nav-item nav-link" to="/rentals">
-            Rentals
-          </NavLink>
-          {!user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/login">
-                Login
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/register">
-                Register
-              </NavLink>
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/profile">
-                {user.name}
-              </NavLink>
-              <NavLink className="nav-item nav-link" to="/logout">
-                Logout
-              </NavLink>
-            </React.Fragment>
-          )}
         </div>
       </div>
     </nav>
