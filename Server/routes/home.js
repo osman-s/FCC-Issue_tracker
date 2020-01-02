f126e84acc40fa31b47540191b672c156fe83115
Osman S. Hasan <54463882+osman-s@users.noreply.github.com>
2020-01-01 19:32:59 -0500

    Rest client changes and post model initiated

--- Server/routes/home.js
+++ Server/routes/home.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-const express = require('express');
-const router = express.Router()
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const express = require("express");
+const router = express.Router();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.post('/api/convert/', urlencodedParser, function(req, res){
-    const data = '33'
-    console.log(req.body)
-    const num = Object.keys(req.body)
-    res.send(num)
-  });
-  
+router.get("/", async (req, res) => {
+  // const user = await User.find({}).sort({name: 1}).select('name _id');
+  res.send("hello");
+});
+
+router.post("/api/convert/", urlencodedParser, function(req, res) {
+  const data = "33";
+  console.log(req.body);
+  const num = Object.keys(req.body);
+  res.send(num);
+});
+
 module.exports = router;
